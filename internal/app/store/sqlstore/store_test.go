2899c8846da719186c881853e30d8be4254af439
MrSedan <elpashevs@bk.ru>
2020-02-13 23:36:35 +0300

    UPD for gitlab test

--- internal/app/store/sqlstore/store_test.go
+++ internal/app/store/sqlstore/store_test.go
@@ -12,7 +12,7 @@ var (
 func TestMain(m *testing.M) {
 	databaseURL = os.Getenv("DATABASE_URL")
 	if databaseURL == "" {
-		databaseURL = "host=localhost dbname=myrest_test sslmode=disable"
+		databaseURL = "host=postgres dbname=myrest_test sslmode=disable user=user password=test"
 	}
 	os.Exit(m.Run())
 }
