5637230155cf6b3a3ff9b625a356ada1012ff05d
Sergey Elpashev <elpashevs@bk.ru>
2020-03-05 16:06:55 +0300

    Da

---
+++ backend/internal/app/apiserver/apiserver.go
@@ -0,0 +1,31 @@
+package apiserver
+
+import (
+	"net/http"
+
+	"github.com/MrSedan/restapigoown/backend/internal/app/store/sqlstore"
+	// This is driver for PostgresDB
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+)
+
+// Start is a function for start server
+func Start(config *Config) error {
+	db, err := newDB(config.DatabaseURL)
+	if err != nil {
+		return err
+	}
+	defer db.Close()
+	store := sqlstore.New(db)
+	srv := newServer(store)
+	srv.jwtKey = config.JwtKey
+	return http.ListenAndServe(config.BindAddr, srv)
+}
+
+func newDB(dbURL string) (*gorm.DB, error) {
+	db, err := gorm.Open("postgres", dbURL)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
+}
